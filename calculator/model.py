128ac2d3b11c2e33abef194df2fb23c7902c6638
Alejandra Escobar Chavarriaga <aleja.escobar2501@gmail.com>
2024-07-30 11:15:28 -0500

    add subtract, multiply and divide methods

--- calculator/model.py
+++ calculator/model.py
@@ -4,3 +4,13 @@ class Calculator:
         return a + b
 
 
+    def subtract(self, a: float, b:float) -> float:
+        return a - b
+
+    def multiply(self, a: float, b:float) -> float:
+        return a * b
+
+    def divide(self, a: float, b:float) -> float | str:
+        if b == 0:
+            return "cannot divide by 0!"
+        return a / b
